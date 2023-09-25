47f183dca59238e85c94e696cc00998901924114
Duc26112003 <duc56977@gmail.com>
2023-09-25 23:04:26 +0700

    Update code

--- BLL/Interfaces/IKhachHangBLL.cs
+++ BLL/Interfaces/IKhachHangBLL.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
+
     public partial interface IKhachHangBLL
     {
         KhachHangDTO GetDatabyID(string id);
--- BTL_API/Controllers/KhachHangController.cs
+++ BTL_API/Controllers/KhachHangController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BLL;
+using DTO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BTL_API.Controllers
 {
@@ -7,6 +10,66 @@ namespace BTL_API.Controllers
     [ApiController]
     public class KhachHangController : ControllerBase
     {
+        private IKhachHangBLL _khachHangBLL;
+        public KhachHangController(IKhachHangBLL khachBLL)
+        {
+            _khachHangBLL = khachBLL;
+        }
+        [Route("get-by-id/{id}")]
+        [HttpGet]
+        public IActionResult GetDatabyID(string id)
+        {
+            var khachHang = _khachHangBLL.GetDatabyID(id);
+            if (khachHang == null)
+            {
+                return NotFound("Khách hàng không tồn tại"); // Trả về 404 Not Found nếu không tìm thấy khách hàng
+            }
+            return Ok(khachHang);
+        }
+        [Route("create-khach")]
+        [HttpPost]
+        public IActionResult CreateItem([FromBody] KhachHangDTO model)
+        {
+            _khachHangBLL.Create(model);
+            return Ok(model);
+        }
 
+        [Route("update-khach")]
+        [HttpPut]
+        public IActionResult UpdateItem([FromBody] KhachHangDTO model)
+        {
+            _khachHangBLL.Update(model);
+            return Ok(model);
+        }
+        [Route("search")]
+        [HttpPost]
+        public IActionResult Search([FromBody] Dictionary<string, object> formData)
+        {
+            try
+            {
+                var page = int.Parse(formData["page"].ToString());
+                var pageSize = int.Parse(formData["pageSize"].ToString());
+                string ten_khach = "";
+                if (formData.Keys.Contains("ten_khach") && !string.IsNullOrEmpty(Convert.ToString(formData["ten_khach"]))) { ten_khach = Convert.ToString(formData["ten_khach"]); }
+                string dia_chi = "";
+                if (formData.Keys.Contains("dia_chi") && !string.IsNullOrEmpty(Convert.ToString(formData["dia_chi"]))) { dia_chi = Convert.ToString(formData["dia_chi"]); }
+                long total = 0;
+                var data = _khachHangBLL.Search(page, pageSize, out total, ten_khach, dia_chi);
+                return Ok(
+                    new
+                    {
+                        TotalItems = total,
+                        Data = data,
+                        Page = page,
+                        PageSize = pageSize
+                    }
+                    );
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
+
