4c1084602c880f693dcd1bc06b66998bad3925f7
Duc26112003 <duc56977@gmail.com>
2023-10-10 16:32:48 +0700

    update code

---
+++ DTO/ThongKeKhach.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public  class ThongKeKhach
+    {
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public int SoLuong { get; set; }
+        public Decimal TongGia { get; set; }
+        public string TenKhachHang { get; set; }
+        public string Diachi { get; set; }
+    }
+}
